dcb026a5ef4a681d8e09dd12d770f8be979299ef
Jaiprakash siyyadri <jaiprakash.siyyadri@gramener.org>
2023-12-15 14:23:51 +0530

    enh: dropdown options

--- speech_synthesis.py
+++ speech_synthesis.py
@@ -26,56 +26,57 @@ st.write("Enter text and synthesize it into speech using Azure Cognitive Service
 user_input = st.text_area("Enter text to synthesize", "Hello, world!")
 
 # Dropdown for voice selection
-available_voices = [
-    "Microsoft Server Speech Text to Speech Voice (en-US, JennyMultilingualNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, JennyMultilingualV2Neural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, RyanMultilingualNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, JennyNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, GuyNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, AriaNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, DavisNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, AmberNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, AnaNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, AndrewNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, AshleyNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, BrandonNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, BrianNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, ChristopherNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, CoraNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, ElizabethNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, EmmaNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, EricNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, JacobNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, JaneNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, JasonNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, MichelleNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, MonicaNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, NancyNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, RogerNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, SaraNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, SteffanNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, TonyNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, AIGenerate1Neural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, AIGenerate2Neural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, AndrewMultilingualNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, AvaMultilingualNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, AvaNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, BlueNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, BrianMultilingualNeural)",
-    "Microsoft Server Speech Text to Speech Voice (en-US, EmmaMultilingualNeural)",
-    "Microsoft Server Speech Text to Speech Voice (te-IN, ShrutiNeural)",
-    "Microsoft Server Speech Text to Speech Voice (te-IN, MohanNeural)",
-]
+voice_mapping = {
+    "JennyMultilingualNeural": "Microsoft Server Speech Text to Speech Voice (en-US, JennyMultilingualNeural)",
+    "JennyMultilingualV2Neural": "Microsoft Server Speech Text to Speech Voice (en-US, JennyMultilingualV2Neural)",
+    "RyanMultilingualNeural": "Microsoft Server Speech Text to Speech Voice (en-US, RyanMultilingualNeural)",
+    "JennyNeural": "Microsoft Server Speech Text to Speech Voice (en-US, JennyNeural)",
+    "GuyNeural": "Microsoft Server Speech Text to Speech Voice (en-US, GuyNeural)",
+    "AriaNeural": "Microsoft Server Speech Text to Speech Voice (en-US, AriaNeural)",
+    "DavisNeural": "Microsoft Server Speech Text to Speech Voice (en-US, DavisNeural)",
+    "AmberNeural": "Microsoft Server Speech Text to Speech Voice (en-US, AmberNeural)",
+    "AnaNeural": "Microsoft Server Speech Text to Speech Voice (en-US, AnaNeural)",
+    "AndrewNeural": "Microsoft Server Speech Text to Speech Voice (en-US, AndrewNeural)",
+    "AshleyNeural": "Microsoft Server Speech Text to Speech Voice (en-US, AshleyNeural)",
+    "BrandonNeural": "Microsoft Server Speech Text to Speech Voice (en-US, BrandonNeural)",
+    "BrianNeural": "Microsoft Server Speech Text to Speech Voice (en-US, BrianNeural)",
+    "ChristopherNeural": "Microsoft Server Speech Text to Speech Voice (en-US, ChristopherNeural)",
+    "CoraNeural": "Microsoft Server Speech Text to Speech Voice (en-US, CoraNeural)",
+    "ElizabethNeural": "Microsoft Server Speech Text to Speech Voice (en-US, ElizabethNeural)",
+    "EmmaNeural": "Microsoft Server Speech Text to Speech Voice (en-US, EmmaNeural)",
+    "EricNeural": "Microsoft Server Speech Text to Speech Voice (en-US, EricNeural)",
+    "JacobNeural": "Microsoft Server Speech Text to Speech Voice (en-US, JacobNeural)",
+    "JaneNeural": "Microsoft Server Speech Text to Speech Voice (en-US, JaneNeural)",
+    "JasonNeural": "Microsoft Server Speech Text to Speech Voice (en-US, JasonNeural)",
+    "MichelleNeural": "Microsoft Server Speech Text to Speech Voice (en-US, MichelleNeural)",
+    "MonicaNeural": "Microsoft Server Speech Text to Speech Voice (en-US, MonicaNeural)",
+    "NancyNeural": "Microsoft Server Speech Text to Speech Voice (en-US, NancyNeural)",
+    "RogerNeural": "Microsoft Server Speech Text to Speech Voice (en-US, RogerNeural)",
+    "SaraNeural": "Microsoft Server Speech Text to Speech Voice (en-US, SaraNeural)",
+    "SteffanNeural": "Microsoft Server Speech Text to Speech Voice (en-US, SteffanNeural)",
+    "TonyNeural": "Microsoft Server Speech Text to Speech Voice (en-US, TonyNeural)",
+    "AIGenerate1Neural": "Microsoft Server Speech Text to Speech Voice (en-US, AIGenerate1Neural)",
+    "AIGenerate2Neural": "Microsoft Server Speech Text to Speech Voice (en-US, AIGenerate2Neural)",
+    "AndrewMultilingualNeural": "Microsoft Server Speech Text to Speech Voice (en-US, AndrewMultilingualNeural)",
+    "AvaMultilingualNeural": "Microsoft Server Speech Text to Speech Voice (en-US, AvaMultilingualNeural)",
+    "AvaNeural": "Microsoft Server Speech Text to Speech Voice (en-US, AvaNeural)",
+    "BlueNeural": "Microsoft Server Speech Text to Speech Voice (en-US, BlueNeural)",
+    "BrianMultilingualNeural": "Microsoft Server Speech Text to Speech Voice (en-US, BrianMultilingualNeural)",
+    "EmmaMultilingualNeural": "Microsoft Server Speech Text to Speech Voice (en-US, EmmaMultilingualNeural)",
+    "ShrutiNeural": "Microsoft Server Speech Text to Speech Voice (te-IN, ShrutiNeural)",
+    "MohanNeural": "Microsoft Server Speech Text to Speech Voice (te-IN, MohanNeural)",
+}
 
-voice_selection = st.selectbox("Choose a voice", available_voices)
+voice_selection = st.selectbox("Choose a voice", list(voice_mapping.keys()))
 
 
 # Function to perform speech synthesis
-def speech_synthesis_to_wave_file(text, voice):
+def speech_synthesis_to_wave_file(text, simplified_voice):
+    full_voice_name = voice_mapping[simplified_voice]
     speech_config = speechsdk.SpeechConfig(
         subscription=speech_key, region=service_region
     )
-    speech_config.speech_synthesis_voice_name = voice
+    speech_config.speech_synthesis_voice_name = full_voice_name
 
     file_name = "outputaudio.wav"
     file_config = speechsdk.audio.AudioOutputConfig(filename=file_name)
